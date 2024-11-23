344a40072e5564d3e0c2cc2d1da10a97d9fcb8e1
CKATEPTb <ckateptb@vivaldi.net>
2024-11-23 10:39:02 +0200

    Schedulers demo

--- packages/reactive/src/index.ts
+++ packages/reactive/src/index.ts
@@ -4,3 +4,16 @@ export type {default as Publisher} from '@/publisher/Publisher'
 export {default as CorePublisher} from '@/publisher/CorePublisher'
 export {default as Mono} from '@/publisher/mono/Mono'
 export {default as Flux} from '@/publisher/flux/Flux'
+
+import Schedulers from "schedulers";
+
+Schedulers.delay(1000).schedule(() => console.log("delay")) // вызвать с задержкой
+Schedulers.macro().schedule(() => console.log("macro")) // вызвать асинхронно
+Schedulers.async().schedule(() => console.log("async")) // вызвать асинхронно, как можно скорее
+Schedulers.immediate().schedule(() => console.log("immediate")) // вызвать прямо сейчас в том потоке, в котором находится.
+// console output:
+//   immediate
+//   async
+//   macro
+//   delay
+
