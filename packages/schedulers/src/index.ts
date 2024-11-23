f7ad2bb4922b528090cd7381d2b0bd3cb2decd57
CKATEPTb <ckateptb@vivaldi.net>
2024-11-23 10:00:48 +0200

    improve schedulers

--- packages/schedulers/src/index.ts
+++ packages/schedulers/src/index.ts
@@ -2,20 +2,32 @@ type Task = () => void
 export declare type Scheduler = {
     schedule: (task: Task) => void;
 }
+export declare type CancellableScheduler = {
+    schedule: (task: Task) => { cancel: () => void };
+} & Scheduler
 
 export default {
-    delay(ms: number): Scheduler {
+    delay(ms: number): CancellableScheduler {
         return {
-            schedule: runnable => setTimeout(runnable, ms)
+            schedule: runnable => {
+                let id = setTimeout(runnable, ms)
+                return {
+                    cancel: () => {
+                        if (id != null) {
+                            clearTimeout(id)
+                        }
+                    }
+                }
+            }
         }
+    },
+    macro(): CancellableScheduler {
+        return this.delay(0)
     },
     async(): Scheduler {
         return {
             schedule: Promise.prototype.then.bind(Promise.resolve())
         }
-    },
-    macro(): Scheduler {
-        return this.delay(0)
     },
     immediate(): Scheduler {
         return {
